bb2606b675228d43b4380f75249e0280ac6cc90e
BasetHamza <75188299+BasetHamza@users.noreply.github.com>
2021-07-08 07:36:50 -0400

    updates the way the li element is created and responding to buttons and delete buttons are working

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,7 @@
 
 // Item counter
 let itemNumber = 1;
+let todolist = document.querySelector('#todo-list'); // Selecting the todo list ul to be modified by adding the an li for each section.
 
 /**
  * Begin Main Functions
@@ -29,22 +30,73 @@ function addTodoItem(evt) {
         //       <i class="fas fa-trash-alt"></i>
         //     </button>
         //   </div > -->
-        var itemToBeAdded = `<div id = "item_${itemNumber}" class="todo-item-container">
-            <li class="todo-item">${itemText}</li>
-            <button class="check-item">
-                <i class="fas fa-check-square"></i>
-            </button>
-            <button class="delete-item">
-                <i class="fas fa-trash-alt"></i>
-            </button>
-        </div > `;
 
-        document.getElementById('todo-list').innerHTML += itemToBeAdded;
+
+        // Creating the check button
+        const newCheckButton = document.createElement('button');
+        newCheckButton.innerHTML = `<i class="far fa-square"></i> `;
+        newCheckButton.setAttribute('id',`item_${itemNumber}_check`);
+        newCheckButton.setAttribute('class',"btn");
+        newCheckButton.setAttribute('class',"check-item");
+
+        // Creating the delete button
+        const newDeleteButton = document.createElement('button');
+        newDeleteButton.innerHTML = `<i class="fas fa-trash-alt"></i>`;
+        newDeleteButton.setAttribute('id',`item_${itemNumber}_delete`);
+        newDeleteButton.setAttribute('class',"btn");
+        newDeleteButton.setAttribute('class',"delete-item");
+
+        // Creating the li element
+        const newTodoItem = document.createElement('li');
+        newTodoItem.setAttribute('class',"unchecked-todo-item");
+        newTodoItem.setAttribute('id',`item_${itemNumber}`);
+        newTodoItem.innerHTML = itemText + newCheckButton.outerHTML + newDeleteButton.outerHTML;
+
+
+        todolist.appendChild(newTodoItem);
+
+            // var itemToBeAdded = `<li class="unchecked-todo-item">${itemText}
+            //     <button id = "item_${itemNumber}_check" class="btn check-item">
+            //         <i class="far fa-square"></i>    
+            //     </button>
+            //     <button id = "item_${itemNumber}_delete" class="btn delete-item">
+            //         <i class="fas fa-trash-alt"></i>
+            //     </button>
+            //     </li>`;
+
+
+        // document.getElementById('todo-list').innerHTML += itemToBeAdded;
 
         itemNumber++;
     }
 }
 
+
+// A function that detects the button clicked and acts accordingly.
+function respondToTheClick(evt) {
+
+    evt.preventDefault(); // Preventing the default action (which is for the button to jump to itself based on href='#') - Is there a better way to do that?
+
+    const clickedBtnID = evt.target.id; // Extracting the id of the button clicked.
+
+    // Now that we detect the clicks on the buttons, we need to indentify wheather it is a delete click or check click.
+    if (clickedBtnID.endsWith("delete"))
+    {
+        const idOfTodoItem = clickedBtnID.substring(0, clickedBtnID.length - 7); // removing the '_btn' from the id to get the section id we need to navigate to and make "active".
+        deleteTodoItem(idOfTodoItem);
+    } else if (clickedBtnID.endsWith("check"))
+    {
+        console.log("check");
+            // <i class="fas fa-check-square"></i>
+
+    }
+}
+
+
+// A function to delete the Todo item knowing its id.
+function deleteTodoItem(idOfTodoItem){
+    todolist.removeChild(document.getElementById(idOfTodoItem));
+}
 
 /**
  * Begin Events
@@ -52,3 +104,7 @@ function addTodoItem(evt) {
 
 // Event Listener for the 'todo-button' click
 document.querySelector('#todo-button').addEventListener('click', addTodoItem);
+
+
+// Event Listener for the delete button
+document.querySelector('#todo-list').addEventListener('click', respondToTheClick);
